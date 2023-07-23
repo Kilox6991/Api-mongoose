0f78b37ef0872a94b39fc6071d722cf22757221f
Kilox6991 <4inf.mpromero@gmail.com>
2023-07-23 19:36:17 +0200

    isAdmin, isAuth

--- src/routes/category.js
+++ src/routes/category.js
@@ -2,14 +2,16 @@ const express = require('express');
 const Category = require('../models/category.js');
 
 const categoryController = require('../controllers/categorys');
+const isAuth = require('../middlewares/isAuth');
+const isAdmin = require('../middlewares/isAdmin');
 const { route } = require('./user.js');
 
 const router = express.Router()
 //Crear nueva categoria
-router.post('/Tienda', categoryController.createCategory);
+router.post('/Tienda',isAuth ,isAdmin, categoryController.createCategory);
 //Actualizar nuevo producto
-router.put('/Tienda/:idCategory', categoryController.updateCategory);
+router.put('/Tienda/:idCategory',isAuth ,isAdmin, categoryController.updateCategory);
 //Borrar category
-router.delete('/Tienda/:idCategory', categoryController.deleteCategory)
+router.delete('/Tienda/:idCategory',isAuth ,isAdmin, categoryController.deleteCategory)
 
 module.exports = router;
--- src/routes/product.js
+++ src/routes/product.js
@@ -2,15 +2,17 @@ const express = require('express');
 const Product = require('../models/product.js');
 
 const productController = require('../controllers/products');
+const isAuth = require('../middlewares/isAuth');
+const isAdmin = require('../middlewares/isAdmin');
 const { route } = require('./user.js');
 
 const router = express.Router()
 //Crear nuevo producto
-router.post('/Tienda', productController.createProduct);
+router.post('/Tienda',isAuth ,isAdmin , productController.createProduct);
 //Actualizar nuevo producto
-router.put('/Tienda/:idProducto', productController.updateProduct);
+router.put('/Tienda/:idProducto',isAuth,isAdmin, productController.updateProduct);
 //Borrar producto
-router.delete('/Tienda/:idProducto', productController.deleteProduct)
+router.delete('/Tienda/:idProducto',isAuth,isAdmin, productController.deleteProduct)
 //Ver todos los productos
 router.get('/Tienda', productController.allProducts)
 //Ver un producto
